887ff5b5727effe9d3dec6343a5905fa55a73b52
CarlosSandoval2003 <jawicho@outlook.com>
2023-04-24 12:17:01 -0600

    Creación del codigo SQL

--- PrototipoP32P2023/src/main/java/scripts/Modulo_Seguridad_Final.sql
+++ PrototipoP32P2023/src/main/java/scripts/Modulo_Seguridad_Final.sql
@@ -1,5 +1,5 @@
-CREATE SCHEMA IF NOT EXISTS `proyectop312023` DEFAULT CHARACTER SET utf8 ;
-USE `proyectop312023` ;
+CREATE SCHEMA IF NOT EXISTS `PrototipoP32P2023` DEFAULT CHARACTER SET utf8 ;
+USE `PrototipoP32P2023` ;
 
 CREATE TABLE IF NOT EXISTS tbl_tipousuario (
 	tipuid INT NOT NULL AUTO_INCREMENT,
@@ -24,57 +24,16 @@ CREATE TABLE IF NOT EXISTS tbl_usuario (
     )
 ENGINE = InnoDB CHARACTER SET = latin1;
 
-CREATE TABLE IF NOT EXISTS tbl_perfil (
-    perid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    pernombre VARCHAR(30) NOT NULL,
-    perestatus VARCHAR (30) NOT NULL
-)ENGINE=InnoDB CHARACTER SET = latin1;
 
-CREATE TABLE IF NOT EXISTS tbl_aplicacion (
-	aplid int NOT NULL AUTO_INCREMENT,
-	aplnombre VARCHAR(50),
-	aplestatus VARCHAR(50),
-	PRIMARY KEY (aplid)
+
+CREATE TABLE IF NOT EXISTS tbl_vendedores (
+	venid int NOT NULL AUTO_INCREMENT,
+	vennombre VARCHAR(50),
+	vennumeroventas int NOT NULL,
+        venareaventas VARCHAR(50),
+	PRIMARY KEY (venid)
 ) ENGINE=InnoDB CHARACTER SET = latin1;    
 
-CREATE TABLE IF NOT EXISTS tbl_modulo (
-	modid INT NOT NULL AUTO_INCREMENT,
-	modnombre VARCHAR(60) NOT NULL,
-	modestatus VARCHAR(60) NOT NULL,
-	PRIMARY KEY (modid) )
-ENGINE = InnoDB CHARACTER SET = latin1;
-
-CREATE TABLE IF NOT EXISTS tbl_aplicacionmodulo (
-	aplid INT NOT NULL,
-    modid INT NOT NULL,
-    PRIMARY KEY (aplid, modid),
-    FOREIGN KEY (aplid) REFERENCES tbl_aplicacion ( aplid),
-    FOREIGN KEY (modid) REFERENCES tbl_modulo (modid) )
-    ENGINE = InnoDB CHARACTER SET = latin1;
-    
- CREATE TABLE IF NOT EXISTS tbl_aplicacionperfil(
-	aplid INT NOT NULL,
-    perid INT NOT NULL,
-    PRIMARY KEY(aplid, perid),
-    FOREIGN KEY (aplid) REFERENCES tbl_aplicacion (aplid),     
-    FOREIGN KEY (perid) REFERENCES tbl_perfil (perid)
- ) ENGINE=InnoDB CHARACTER SET = latin1;     
-
-CREATE TABLE IF NOT EXISTS tbl_aplicacionusuario (
-	aplid INT NOT NULL,
-	usuid INT NOT NULL,
-	PRIMARY KEY (aplid, usuid), 
-	FOREIGN KEY (aplid) references tbl_aplicacion (aplid),
-	FOREIGN KEY (usuid) references tbl_usuario (usuid)
-) ENGINE = InnoDB CHARACTER SET = latin1;
-
-CREATE TABLE IF NOT EXISTS tbl_perfilusuario (
-	perid int NOT NULL,
-	usuid int NOT NULL,
-	PRIMARY KEY (perid, usuid),
-	FOREIGN KEY (perid) REFERENCES tbl_perfil (perid),
-	FOREIGN KEY (usuid) REFERENCES tbl_usuario (usuid)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS tbl_bitacora (
     bitid int auto_increment PRIMARY KEY,
